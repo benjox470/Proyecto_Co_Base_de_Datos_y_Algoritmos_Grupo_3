ad343927dd3cc885e49d279b1c731afa32b68f10
Brayan <brayan.cardenaset32@gmail.com>
2025-09-09 15:52:25 -0300

    Create Prueba_Cardozo.sql

---
+++ Prueba_Cardozo.sql
@@ -0,0 +1,6 @@
+insert into Clientes (id_cliente,Nombre,Apellido,DNI,historial_compras,direccion,edad) values
+(001,"Salomon","Rondon","444576008","condones prime","Teodoro Garcia",19),
+(002,"Rodrigo", "Bueno", "23328352", "condones elite","Av. venezuela",52),
+(003,"thiago","da cruz","48653007","prueba de embarazo
+(004),
+(005);
