97c51baf2ac5b3405b37b36a731c34a4d09731c2
Benjox4700 <benjamin.saragustiet32@gmail.com>
2025-09-12 15:10:18 -0300

    Delete Prueba_Cardozo.sql

--- Prueba_Cardozo.sql
+++
@@ -1,6 +0,0 @@
-insert into Clientes (id_cliente,Nombre,Apellido,DNI,historial_compras,direccion,edad) values
-(001,"Salomon","Rondon","444576008","condones prime","Teodoro Garcia",19),
-(002,"Rodrigo", "Bueno", "23328352", "condones elite","Av. venezuela",52),
-(003,"thiago","da cruz","48653007","prueba de embarazo
-(004),
-(005);
