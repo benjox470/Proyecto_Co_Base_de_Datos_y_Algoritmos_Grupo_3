78f7d15ba52a8d6b870eb2f1d2ad6e047655bd26
PabloCardozo224 <pblodcardo@gmail.com>
2025-09-23 15:56:23 -0300

    Add files via upload

---
+++ Sql/Cardozo.sql
@@ -0,0 +1,13 @@
+insert into Elemento_gondola (id_medicamento, Descripcion) values
+(1,"taquilla llena"),
+(2,"taquilla llena"),
+(3,"disponible"),
+(4, "disponible"),
+(5, "taquilla llena");
+
+
+
+insert into Elementos_Fondo (id_medicamento, descripcion) values
+(6,"taquilla llena"),
+(7,"taquilla llena"),
+(
