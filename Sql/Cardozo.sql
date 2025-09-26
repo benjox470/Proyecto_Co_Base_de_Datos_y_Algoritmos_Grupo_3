2902d3a5be3d771a8db93d6737b87531c8e4d2b6
PabloCardozo224 <pblodcardo@gmail.com>
2025-09-26 14:05:20 -0300

    Update Cardozo.sql

--- Sql/Cardozo.sql
+++ Sql/Cardozo.sql
@@ -1,13 +1,19 @@
-insert into Elemento_gondola (id_medicamento, Descripcion) values
-(1,"taquilla llena"),
+create table Elementos_gondola (
+	Id_gondola int (3) unsigned auto_increment primary key,
+    Id_medicamentos int (3) unsigned,
+
 (2,"taquilla llena"),
-(3,"disponible"),
-(4, "disponible"),
-(5, "taquilla llena");
+(4,"taquilla llena"),
+(5,"disponible"),
+(2, "disponible"),
+(4, "taquilla llena");
+
 
 
 
 insert into Elementos_Fondo (id_medicamento, descripcion) values
-(6,"taquilla llena"),
-(7,"taquilla llena"),
-(
+(2,"disponibles"),
+(4,"disponibles"),
+(5,"no disponible"),
+(2,"disponible"),
+(4, "no disponible");
