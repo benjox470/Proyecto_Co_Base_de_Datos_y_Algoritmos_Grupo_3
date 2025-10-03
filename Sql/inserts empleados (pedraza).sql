69a1e2c9df9656347e5bd7913bce95c9e5ccb547
santiago-PC <santiago.pedrazaclericiet32@gmail.com>
2025-10-03 14:32:15 -0300

    Add files via upload

---
+++ Sql/inserts empleados (pedraza).sql
@@ -0,0 +1,30 @@
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(1, "Norberto González", 2500.1, 1);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(2, "Ana María Soldano", 2100.22, 2);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(3, "Marcos Alonso De las Nieves", 1905.12, 3);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(4, "Gonzalo Agustín Celman", 2410.0, 4);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(5, "Juana Sofía Meza", 1420.66, 5);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(6, "Lucio Sepeda", 3000.50, 6);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(7, "Manuela Castillo", 1780.0, 7);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(8, "Manuela Castillo", 2850.0, 8);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(9, "Miguel Velázquez", 2000.3, 9);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(10, "Carmen María Blázquez", 3000.12, 10);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(11, "Denis Ferrandis", 2390.1, 11);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(12, "Ana Isabel Gabasa", 2005.44, 12);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(13, "Juan Francisco Torres", 1600.33, 13);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(14, "Yasmin Alcaide", 2300.33, 14);
+insert into Empleados(Id_role, Nombre, Sueldo, Sector)
+values(15, "Eric García", 1802.14, 15);
